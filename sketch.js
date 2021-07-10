cba65ba7c0f428acf549bb74edd2bafe163a7373
nysa1312 <86355089+nysa1312@users.noreply.github.com>
2021-07-10 13:35:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var car , wall ;
+var speed , weight ;
+
+
+function setup() {
+  createCanvas(1600,400);
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500, 200, 60, height/2)
+  wall.shapeColor(80,80,80)
+  speed = random(55,90);
+  weight = random(400, 1500);
+  car.velocityX = speed;
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
